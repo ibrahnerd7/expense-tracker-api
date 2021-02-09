53c5ac7ffd7d57187506c5ab1ee4c10206a90929
ibrahnerd7 <ibrahmaina7@gmail.com>
2021-02-09 19:12:21 +0300

    Create user controller

---
+++ api/users/users.controller.js
@@ -0,0 +1,77 @@
+const express=require('express');
+const router=express.Router();
+const Joi=require('joi');
+const validateRequest=require('../_middleware/validate-request');
+const authorize=require('../_middleware/authorize');
+const userService=require('../users/users.service');
+
+//routes
+router.post()
+
+function authenticateSchema(req, res, next) {
+    const schema = Joi.object({
+        userName: Joi.string().required(),
+        password: Joi.string().required()
+    });
+    validateRequest(req, next, schema);
+}
+
+function authenticate(req, res, next) {
+    userService.authenticate(req.body)
+        .then(user => res.json(user))
+        .catch(next);
+}
+
+function registerSchema(req, res, next) {
+    const schema = Joi.object({
+        userName: Joi.string().required(),
+        email: Joi.string().required(),
+        picture: Joi.string().required(),
+        password: Joi.string().min(6).required()
+    });
+    validateRequest(req, next, schema);
+}
+
+function register(req, res, next) {
+    userService.create(req.body)
+        .then(() => res.json({ message: 'Registration successful' }))
+        .catch(next);
+}
+
+function getAll(req, res, next) {
+    userService.getAll()
+        .then(users => res.json(users))
+        .catch(next);
+}
+
+function getCurrent(req, res, next) {
+    res.json(req.user);
+}
+
+function getById(req, res, next) {
+    userService.getById(req.params.id)
+        .then(user => res.json(user))
+        .catch(next);
+}
+
+function updateSchema(req, res, next) {
+    const schema = Joi.object({
+        userName: Joi.string().empty(''),
+        email: Joi.string().empty(''),
+        picture: Joi.string().empty(''),
+        password: Joi.string().min(6).empty('')
+    });
+    validateRequest(req, next, schema);
+}
+
+function update(req, res, next) {
+    userService.update(req.params.id, req.body)
+        .then(user => res.json(user))
+        .catch(next);
+}
+
+function _delete(req, res, next) {
+    userService.delete(req.params.id)
+        .then(() => res.json({ message: 'User deleted successfully' }))
+        .catch(next);
+}
