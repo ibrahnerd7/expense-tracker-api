c74d6ef97ae858073560eb85fc25508c891aadb2
ibrahnerd7 <ibrahmaina7@gmail.com>
2021-02-10 11:33:12 +0300

    Apply swagger documentation middleware

--- server.js
+++ server.js
@@ -4,12 +4,15 @@ const app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const errorHandler = require('./api/_middleware/error-handler');
+const swaggerUi =require('swagger-ui-express');
+const swaggerDocument=require('./api/swagger.config.json')
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
 
 // api routes
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use('/users', require('./api/users/users.controller'));
 app.use('/expenses',require('./api/expenses/expense.controller'))
 
